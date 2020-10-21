d870923bb180eefc6d86cdf9c9b454dd3282b300
Manuel Zegarra <mzegarra@mzegarra.com>
2020-10-21 01:18:47 -0500

    Update

--- src/main/java/com/example/lab04/exceptions/InPanicException.java
+++ src/main/java/com/example/lab04/exceptions/InPanicException.java
@@ -2,7 +2,7 @@ package com.example.lab04.exceptions;
 
 public class InPanicException extends RuntimeException {
 
-    private static final long serialVersionUID = 6922159999398000261L;
+    //private static final long serialVersionUID = 6922159999398000261L;
 
     public InPanicException() {
         super("In Panic attack");
